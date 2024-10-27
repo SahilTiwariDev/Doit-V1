910a619db44348f871f8863a6d350d5b682fe317
SahilTiwariDev <sahiltiwari.contactme@gmail.com>
2024-10-27 18:46:12 +0530

    Add color code for styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      "mint-green": "#E9F5DB",
+      "light-sage": "#CFE1B9",
+      "sage-green": "#B5C99A",
+      "olive-green": "#97A97C",
+      "moss-green": "#87986A",
+      "forest-green": "#718355",
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
