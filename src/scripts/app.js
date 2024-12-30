da14dee873aa424c9f46e0948f01bd9f5874fb14
SahilTiwariDev <sahiltiwari.contactme@gmail.com>
2024-12-30 16:01:54 +0530

    Add a pop-up window for showing empty input field messege

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -5,15 +5,25 @@ let descriptionOfTask = document.getElementById("taskDescription");
 let taskArea = document.getElementById("displayArea");
 
 function addTask(name, description) {
-  let taskbody = document.createElement("div");
-  let taskFront = document.createElement("p");
-  let taskBackt = document.createElement("p");
-  taskbody.setAttribute("class", "flex");
-  taskFront.innerHTML = name.value;
-  taskBackt.innerHTML = description.value;
-  taskbody.appendChild(taskFront);
-  taskbody.appendChild(taskBackt);
-  taskArea.appendChild(taskbody);
+  if (description.value === "") {
+    let popUpWindow = document.getElementById("popUpBox");
+    popUpWindow.classList.remove("hidden");
+    popUpWindow.classList.add("flex");
+    document.getElementById("closePopUpButton").onclick = () => {
+      popUpWindow.classList.add("hidden");
+      popUpWindow.classList.remove("flex");
+    };
+  } else {
+    let taskbody = document.createElement("div");
+    let taskFront = document.createElement("p");
+    let taskBackt = document.createElement("p");
+    taskbody.setAttribute("class", "flex");
+    taskFront.innerHTML = name.value;
+    taskBackt.innerHTML = description.value;
+    taskbody.appendChild(taskFront);
+    taskbody.appendChild(taskBackt);
+    taskArea.appendChild(taskbody);
+  }
 }
 
 document.getElementById("addButton").onclick = () => {
